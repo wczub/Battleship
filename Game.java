5a97019c728d4d54603b6648d09bd6851f0e2171
codewolf <willczubiak@gmail.com>
2017-03-22 22:22:41 +0000

    deleted a lot of irrelivant code

--- Game.java
+++ Game.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 
 public class Game{
     Player p1;
-    Player p2;
     Computer c;
     boolean comp;
     
     
-    Game(int x){
+    Game(){
         
         String name = "";
         
@@ -18,47 +17,11 @@ public class Game{
         name = scanner.next();
         
         p1 = new Player(name);
+        c = new Computer();
         
-        
-        if (x == 1){
-            p2 = new Player();
-            comp = false;
-        } else {
-            c = new Computer();
-            comp = true;
-        }
     }  
     
-    // Starts the game for two player
-    public void startTwo(){
-        
-        boolean p1Win = false, p2Win = false;
-        
-        while (true){
-            
-            // a player takes a turn, and sees if the other player lost
-            // if the other player lost, then it breaks the loop
-            turn(0);
-            p1Win = p2.lose();
-            if (p1Win) 
-                break;
-            
-            turn(1);
-            p2Win = p1.lose();
-            if (p2Win)
-                break;
-            
-        }
-        
-        if (p1Win){
-            System.out.printf("Congratulations %s! You beat %s!\n", % p1.getName(), p2.getName());
-        } else {
-            System.out.printf("Congratulations %s! You beat %s!\n", % p2.getName(), p1.getName());
-        }
-        
-    }
-    
-    public void startComp(){
+    public void start(){
         
         boolean p1Win = false, cWin = false;
         
